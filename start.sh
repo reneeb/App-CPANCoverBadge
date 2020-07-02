2e5b62e453522b6a38eacd08c05a91b5f29842b9
reneeb <info@perl-services.de>
2020-07-02 08:39:09 +0200

    add files to run application as a service

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source ~/perl5/perlbrew/etc/bashrc
+perlbrew use 5.28.1
+cd ~/App-CPANCoverBadge
+
+if [ $1 = "start" ]; then
+    hypnotoad bin/cpan_cover_badge -f
+elif [ $1 = "restart"]; then
+    hypnotoad bin/cpan_cover_badge
+elif [ $1 = "stop" ]; then
+    hypnotoad -s bin/cpan_cover_badge
+fi
